cae446acb7cfe246395531a6cae2a52236444f33
herbicus <sonofman_x@hotmail.com>
2015-04-13 23:17:53 -0400

    fixed nav and figured out horizontal waypoints

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,92 +6,89 @@ var cornerstoneAPI = (function(options) {
 		options = options || {}
 	
 	// PRELOADER UTILITY
-		var AceLoadImages = AceLoadImages || function(a, d, z) {
-			a instanceof Array || (a = [a]);
-			for (var e = a.length, f = 0, g = e; g--;) {
-				var b = document.createElement("img");
-				b.onload = function() {
-					f++; 
-					f >= e && isFunction(d) && d(z)
-				};
-				b.src = a[g]; 
-			}
+	var AceLoadImages = AceLoadImages || function(a, d, z) {
+		a instanceof Array || (a = [a]);
+		for (var e = a.length, f = 0, g = e; g--;) {
+			var b = document.createElement("img");
+			b.onload = function() {
+				f++; 
+				f >= e && isFunction(d) && d(z)
+			};
+			b.src = a[g]; 
 		}
+	}
 
-		var isFunction = isFunction || function(functionToCheck) {
-			var getType = {};
-			return functionToCheck && getType.toString.call(functionToCheck) == '[object Function]';
-		}
+	var isFunction = isFunction || function(functionToCheck) {
+		var getType = {};
+		return functionToCheck && getType.toString.call(functionToCheck) == '[object Function]';
+	}	
 
-		
+	AceLoadImages([
+		'assets/img/planes/NASM Illustration_v4-09.png',
+		'assets/img/planes/NASM Illustration_v4-10.png',
+		'assets/img/planes/NASM Illustration_v4-11.png',
+		'assets/img/planes/NASM Illustration_v4-12.png',
+		'assets/img/planes/NASM Illustration_v4-13.png',
+		'assets/img/planes/NASM Illustration_v4-14.png',
+		'assets/img/planes/NASM Illustration_v4-15.png',
+		'assets/img/bg/bg-apollo.png',
+		'assets/img/bg/bg-last-transform-blur.jpg',
+		'assets/img/bg/bg-last-transform-norm.jpg',
+		'assets/img/bg/bg-mustang.png',
+		'assets/img/bg/bg-rover.png',
+		'assets/img/bg/bg-shuttle.png',
+		'assets/img/bg/bg-spirit.png',
+		'assets/img/bg/bg-splash.png',
+		'assets/img/bg/bg-sputnik.png',
+		'assets/img/bg/bg-station.png',
+		'assets/img/bg/bg-wright.png',
+	], imagesAreLoaded);
 
-
-		AceLoadImages([
-			'assets/img/planes/NASM Illustration_v4-09.png',
-			'assets/img/planes/NASM Illustration_v4-10.png',
-			'assets/img/planes/NASM Illustration_v4-11.png',
-			'assets/img/planes/NASM Illustration_v4-12.png',
-			'assets/img/planes/NASM Illustration_v4-13.png',
-			'assets/img/planes/NASM Illustration_v4-14.png',
-			'assets/img/planes/NASM Illustration_v4-15.png',
-			'assets/img/bg/bg-apollo.png',
-			'assets/img/bg/bg-last-transform-blur.jpg',
-			'assets/img/bg/bg-last-transform-norm.jpg',
-			'assets/img/bg/bg-mustang.png',
-			'assets/img/bg/bg-rover.png',
-			'assets/img/bg/bg-shuttle.png',
-			'assets/img/bg/bg-spirit.png',
-			'assets/img/bg/bg-splash.png',
-			'assets/img/bg/bg-sputnik.png',
-			'assets/img/bg/bg-station.png',
-			'assets/img/bg/bg-wright.png',
-		], imagesAreLoaded);
-
-		function imagesAreLoaded() {
-		  	TweenMax.to(".l-splash-overlay" , 0.75, {
-		  		autoAlpha: 0,
-		  		display: "none",
-		  		ease: Back.easeInOut,
-  		  		onComplete: function() {
-  		  			TweenMax.to(".l-section-splash img" , 0.5, {
-  				  		autoAlpha: 1,
-  				  		left: "50%",
-  				  		top: "50%",
-  				  		ease: Back.easeInOut
-  				  	});
-  		  		}
-		  	});
-		}
-		// END PRELOAD UTILITY
+	function imagesAreLoaded() {
+	  	TweenMax.to(".l-splash-overlay" , 0.75, {
+	  		autoAlpha: 0,
+	  		display: "none",
+	  		ease: Back.easeInOut, 		
+	  		onComplete: function() {
+	  			
+	  			TweenMax.to(".l-section-splash img" , 0.5, {
+			  		autoAlpha: 1,
+			  		left: "50%",
+			  		top: "50%",
+			  		ease: Back.easeInOut
+			  	});
+	  		}
+	  	});
+	}
+	// END PRELOAD UTILITY
 
 	// INIT FUNCTION
 	var init = function() {
 		console.log('cornerstone API works');
 		// MOUSEWHEEL
 		$('html, body, *').mousewheel(function(event, delta) {
-			// console.log(event.deltaX, event.deltaY, event.deltaFactor);
-		    this.scrollLeft -= (delta * 30);
-		    this.scrollRight -= (delta * 30);
+		    var speed = 3;
+		    this.scrollLeft -= (delta * speed);
+		    this.scrollRight -= (delta * speed);
 		    event.preventDefault();
 		});
 
+		// stellar for now, knock out after waypoints
 		$(window).stellar();
 
 		// STELLAR JS
 		$.stellar({
-		  horizontalScrolling: true,
-		  responsive: true,
-		  scrollProperty: 'scroll',
-		  positionProperty: 'position'
+			horizontalScrolling: true,
+			responsive: true,
+			scrollProperty: 'scroll',
+			positionProperty: 'position'
 		});
 
 		// MOBILE TOGGLE
-
 		var mobileNav = new TimelineMax({paused: true});
 
 		mobileNav.to(["#mobile-list"], 0.15, {left: 0, ease: Back.easeInOut});
-
-		//mobileNav.pause();
+		
 		// EVENT HANDLER
 		$(".mobile-nav-icon").on("click", function() {
 			console.log("presssed");
@@ -103,32 +100,6 @@ var cornerstoneAPI = (function(options) {
 			}	
 			$(this).toggleClass("played");
 		});
-
-		// $(".l-timeline-nav #section-start").on("mouseover", function(){
-		// 	$(".l-timeline-nav .timeline-gauge").css(' background',' url(../img/other/hovers/start.png) ' );
-		// 	TweenMax.to(".l-timeline-nav .timeline-gauge", 0.75, {
- 	// 	  		css: {"background": "url(../img/other/hovers/start.png)"}
- 	// 		});
-		// });
-
-		// $(function(){
-		// 	$(window).scroll(function(e){
-		// 		if($(this).scrollLeft()> 1000){
-		// 			console.log("Happened");
-		// 			TweenMax.to("#wright-text", 0.75, {
-		// 		  		autoAlpha: 1,
-		// 		  		scale: 1,
-		// 				ease: Back.easeInOut
-		// 			});
-		// 		} else {
-		// 			TweenMax.to("#wright-text", 0.75, {
-		// 		  		autoAlpha: 0,
-		// 		  		scale: 0,
-		// 				ease: Back.easeInOut
-		// 			});
-		// 		}
-		// 	});
-		// });
 
 		// SCROLL TO's
 		$("#section-start").click(function() {
@@ -194,13 +165,25 @@ var cornerstoneAPI = (function(options) {
 
 		$(".l-section-last").on("click", function(){
 			finalSectionTransition.play();
-			
-		})
+		});
 
 		// WHOLE DIV LINK
 		$(".four-col").click(function(){
 		     window.location=$(this).find("a").attr("href"); 
 		     return false;
+		});
+
+		// WAYPOINTS HORIZONTAL PARALLAX STUFF
+		console.log("make waypoints");
+		var waypoint = new Waypoint({
+			element: document.body,
+			horizontal: true,
+			offset: '-100%', // full width of section
+			handler: function(direction) {
+				console.log("section 1 happened");
+				TweenMax.to("#l-section-1 .body-copy", 1, {color: "blue"});
+				// todo: write tweenmax timelines on elements (background, planes, clouds, stars, etc...)
+			}
 		});
 
 		// ANIMATION TIMELINE
@@ -224,5 +207,4 @@ var cornerstoneAPI = (function(options) {
 
 $(document).ready(function() {
 	cornerstoneAPI.init();
-
 });
